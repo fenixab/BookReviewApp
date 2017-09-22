3c3e0a90154d2c6e9c9610b3d83f40a7c167f192
fenixab <fenixab1981@gmail.com>
2017-09-22 11:36:05 +0100

    Begining part 4

--- Gemfile
+++ Gemfile
@@ -19,6 +19,9 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'simple_form', '~> 3.5'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'devise', '~> 4.3'
+gem 'paperclip', '~> 5.1'
+gem 'aws-sdk', '< 2.0'
 
 
 group :development, :test do
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,46 @@
 class BooksController < ApplicationController
+	
 	before_action :find_book, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@books = Book.all.order("created_at DESC")
+		if params[:category].blank?
+			@books = Book.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@books = Book.where(:category_id => @category_id).order("created_at DESC")
+		end
 	end
 
 	def show
-	
+		
 	end
 
 	def new
-		@book = Book.new
+		@book = current_user.books.build
+		@categories = Category.all.map{ |c| [c.name, c.id] }
 	end
 
 	def create
-		@book = Book.new(book_params)
+		@book = current_user.books.build(book_params)
+		@book.category_id = params[:category_id]
 
 		if @book.save
 			redirect_to root_path
 		else
+			@categories = Category.all.map{ |c| [c.name, c.id] }
 			render 'new'
 		end
 	end
 
 	def edit
+		@categories = Category.all.map{ |c| [c.name, c.id] }
 	end
 
 	def update
+		@book.category_id = params[:category_id]
+
 		if @book.update(book_params)
-			redirect_to book_path(@book)
+			redirect_to book_path(@book)	
 		else
 			render 'edit'
 		end
@@ -36,18 +48,21 @@ class BooksController < ApplicationController
 
 	def destroy
 		@book.destroy
-		redirect_to book_path
+		redirect_to root_path
 	end
 
 
 	private
 
 		def book_params
-			params.require(:book).permit(:title, :description, :author)
+			params.require(:book).permit(:title, :description, :author, :category_id, :book_img)
 		end
 
 		def find_book
-			@book = Book.find(params[:id])
+
+				@book = Book.find(params[:id])	
+
+		
 		end
 
 end
